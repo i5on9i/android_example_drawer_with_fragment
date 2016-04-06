6e6f6606079f2137bcbf11772d8d34dd662a8edd
i5on9i <gaedduck@gmail.com>
2016-04-06 19:11:31 +0900

    - onCreateOptionsMenu() / onOptionsItemSelected() are moved to the fragment - Toolbar / FloatingActionButton codes are moved to the fragment

--- app/src/main/java/com/namh/drawerwithfragment/OneFragment.java
+++ app/src/main/java/com/namh/drawerwithfragment/OneFragment.java
@@ -3,8 +3,18 @@ package com.namh.drawerwithfragment;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -18,6 +28,33 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class OneFragment extends Fragment {
+
+
+
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+
+    ////////////////////////////////////////////////////////////
+    //
+    //
+    //  Start here...
+    //
+    //
+    ////////////////////////////////////////////////////////////
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -51,6 +88,14 @@ public class OneFragment extends Fragment {
         return fragment;
     }
 
+    ////////////////////////////////////////////////////////////
+    //
+    //
+    //  Overrides
+    //
+    //
+    ////////////////////////////////////////////////////////////
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -63,16 +108,21 @@ public class OneFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        // inflate
+        View view = inflater.inflate(R.layout.fragment_one, container, false);
+
+        _setAppBar(view);
+        _setFab(view);
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_one, container, false);
+        return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
+
+
+
 
     @Override
     public void onAttach(Context context) {
@@ -91,18 +141,77 @@ public class OneFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+    // Menu
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.main, menu);
+        super.onCreateOptionsMenu(menu, inflater);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }else if (id == android.R.id.home){
+            // open the Drawer menu
+            DrawerLayout drawer = (DrawerLayout) getActivity().findViewById(R.id.drawer_layout);
+            drawer.openDrawer(GravityCompat.START);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    ////////////////////////////////////////////////////////////
+    //
+    //
+    //  methods
+    //
+    //
+    ////////////////////////////////////////////////////////////
+
+    private void _setAppBar(View view) {
+        // toolbar
+        Toolbar toolbar = (Toolbar)view.findViewById(R.id.toolbar);
+        AppCompatActivity atvt = (AppCompatActivity)getActivity();
+        atvt.setSupportActionBar(toolbar);
+
+        // back button
+        ActionBar ab = atvt.getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void _setFab(View view) {
+        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    ////////////////////////////////////////////////////////////
+    //
+    //
+    //  Implements
+    //
+    //
+    ////////////////////////////////////////////////////////////
+
 }

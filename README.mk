36edd1ef3d916ae090bb9567818498c5f0ac576a
i5on9i <gaedduck@gmail.com>
2016-04-06 19:41:31 +0900

    - add README.mk

---
+++ README.mk
@@ -0,0 +1,4 @@
+# Drawer Menu with Fragments
+This is Simple example app with Drawer Menu. In this app, when you click the menu item of the Drawer Menu you can see the current Fragment is replaced with the other Fragment.
+
+In this app, Activity has the Drawer Menu and the fragments have the toolbars.
